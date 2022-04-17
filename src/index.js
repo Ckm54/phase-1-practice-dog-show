e86143b2c26b9582ba49e376f20959d408bf36a9
Collins <mutaic84@gmail.com>
2022-04-17 13:28:29 +0300

    display dog data on table

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const dogTable = document.getElementById("table-body")
 
+    function getDogs() {
+        fetch("http://localhost:3000/dogs")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                createDom(element)
+            });
+        })
+    }
+    
+    function createDom(data) {
+        let row = document.createElement("tr")
+        let rowData = `
+            <td>${data.name}</td>
+            <td>${data.breed}</td>
+            <td>${data.sex}</td>
+            <td><button>Edit</button></td>    
+        `
+        row.innerHTML = rowData
+        dogTable.append(row)
+    }
+
+    getDogs()
 })
